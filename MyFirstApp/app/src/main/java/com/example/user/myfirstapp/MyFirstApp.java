2c6115f4d2d9e398f6ba642e0b8c662194379d8e
Megan Strachan <m.e.strachan@gmail.com>
2016-08-16 13:48:57 +0100

    final submission

---
+++ MyFirstApp/app/src/main/java/com/example/user/myfirstapp/MyFirstApp.java
@@ -0,0 +1,17 @@
+package com.example.user.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 15/08/2016.
+ */
+public class MyFirstApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
